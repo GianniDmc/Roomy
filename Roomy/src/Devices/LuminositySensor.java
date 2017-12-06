c28c7da0774e1105ced5049d1711ae7898d42bf6
paul <paul.guirbal@outlook.fr>
2017-12-06 14:46:26 +0100

    [LuminositySensor] Changement de nom de la classe

---
+++ Roomy/src/Devices/LuminositySensor.java
@@ -0,0 +1,23 @@
+package Devices;
+
+public class LuminositySensor {
+	int id;
+	int luminosity;
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public int getLuminosity(){
+		return this.luminosity;
+	}
+	
+	public void setId(int value){
+		this.id = value;
+	}
+	
+	//Pour les test, on peut affecter une valeur au capteur
+	public void setLuminosity(int value){
+		this.luminosity = value;
+	}
+}
